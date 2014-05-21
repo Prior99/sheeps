e25a1878396dce8764bc321ab40178f0c106d80c
Frederick Gnodtke <fgnodtke@cronosx.de>
2014-05-21 18:25:00 +0200

    Added statusdisplay, fixed tickInterval not to be cleared if game over

--- game.js
+++ game.js
@@ -38,8 +38,9 @@ var Game = {
 				Game.drawables[key].draw();
 			}
 			Game.cursor.draw(ctx);
+			Game.drawInfo();
 		}, 100/FPS);
-		var tickInterval = setInterval(function() {
+		Game.tickInterval = setInterval(function() {
 			for(var key in Game.tickables) {
 				Game.tickables[key].tick();
 			}
@@ -47,17 +48,17 @@ var Game = {
 		}, 100/FPS);
 	},
 	checkWin : function() {
-		var remaining = 0;
+		Game.remaining = 0;
 		var won = true;
 		for(var key in Game.targets) {
 			var target = Game.targets[key];
-			remaining += target.remaining();
+			Game.remaining += target.remaining();
 			if(!target.check()) won = false;
 		}
 		if(won) {
 			Game.win();
 		}
-		if(Game.sheeps.length < remaining) {
+		if(Game.sheeps.length < Game.remaining) {
 			Game.lose();
 		}
 	},
@@ -72,5 +73,12 @@ var Game = {
 	stop : function() {
 		clearInterval(Game.tickInterval);
 		clearInterval(Game.drawInterval);
+	},
+	drawInfo : function() {
+		var ctx = Game.ctx;
+		ctx.fillStyle = "rgb(70, 70, 255)";
+		ctx.font = "bold 16px Arial";
+		ctx.fillText(Game.remaining + " sheeps to rescue", 10, 26);
+		ctx.fillText(Game.sheeps.length + " sheeps alive", 10, 26 + 16 + 5);
 	}
 };
