253a67054628f7bf90468e3c57d781dd95e787d9
Frederick Gnodtke <fgnodtke@cronosx.de>
2014-05-23 17:39:10 +0200

    Added and, added level 11 (Train)

---
+++ js/objects/and.js
@@ -0,0 +1,20 @@
+var And = function(target, inputs) {
+	this.target = target;
+	this.inputs = inputs;
+	this.count = 0;
+}
+
+And.prototype = {
+	activate : function() {
+		this.count++;
+		if(this.count >= this.inputs) {
+			this.target.activate();
+		}
+	},
+	deactivate : function() {
+		this.count--;
+		if(this.count < this.inputs) {
+			this.target.deactivate();
+		}
+	}
+}
---
+++ levels/level_011.js
@@ -0,0 +1,46 @@
+module.exports = {
+	name : "Train",
+	description : "It can be somehow exhausting to row it up a stream all day.", 
+	image : "img/thumb_level_011.png",
+	dependencies : ["Onion"],
+	init : function () {
+		new Target({
+			pos : [400, 550],
+			radius : 20,
+			amount : 1, 
+			sticky :true
+		});
+		new Wall({
+			pos : [0, 275],
+			width: 350,
+			height: 50
+		});
+		new Wall({
+			pos : [450, 275],
+			width: 350,
+			height: 50
+		});
+		var hole = new Hole({
+			pos : [400, 300],
+			radius: 50
+		});
+		var and = new And(new Not(hole), 2);
+		new Button({
+			pos : [550, 100],
+			width: 200,
+			height: 100,
+			targets : [and]
+		});
+		new Button({
+			pos : [50, 100],
+			width: 200,
+			height: 100,
+			targets : [and]
+		});
+		for(var i = 0; i < 5; i++)
+			new Sheep({
+				pos : [200 + 50 * i, 50],
+				drunkmodifier : 0
+			});	
+	}
+};
